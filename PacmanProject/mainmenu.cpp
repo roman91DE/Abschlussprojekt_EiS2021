4e2cd01e2cbed6e6df53e2b43fe1bc6a6f556329
roman91DE <rohoehn123@gmail.com>
2021-02-24 16:28:04 +0100

    addedCustomLevel

--- PacmanProject/mainmenu.cpp
+++ PacmanProject/mainmenu.cpp
@@ -1,18 +1,27 @@
 #include "mainmenu.h"
 #include "ui_mainmenu.h"
 #include <QErrorMessage>
+#include <QValidator>
+
 mainMenu::mainMenu(QWidget *parent) : QMainWindow(parent), ui(new Ui::mainMenu){
     ui->setupUi(this);
-    // relative paths!
+    // setting relative paths for standard levels
     lvl1_path = "../PacmanProject/txt_maps/level1.txt";
     lvl2_path = "../PacmanProject/txt_maps/level2.txt";
     lvl3_path = "../PacmanProject/txt_maps/level3.txt";
-    connect(ui->playLevel1, SIGNAL(clicked()), this, SLOT(slotStartLevel1()));
-    connect(ui->playLevel2, SIGNAL(clicked()), this, SLOT(slotStartLevel2()));
-    connect(ui->playLevel3, SIGNAL(clicked()), this, SLOT(slotStartLevel3()));
-    connect(ui->playCustomLevel, SIGNAL(clicked()), this, SLOT(slotStartCustomLevel()));
+    // connections for main menu buttons
+    connect(ui->playLevel1,         SIGNAL(clicked()), this, SLOT(slotStartLevel1()));
+    connect(ui->playLevel2,         SIGNAL(clicked()), this, SLOT(slotStartLevel2()));
+    connect(ui->playLevel3,         SIGNAL(clicked()), this, SLOT(slotStartLevel3()));
+    connect(ui->playCustomLevel,    SIGNAL(clicked()), this, SLOT(slotStartCustomLevel()));
+    connect(ui->quitButton,         SIGNAL(clicked()), this, SLOT(close()));
+    // setting up tool tips
+    ui->playLevel1->setToolTip("Start Level 1!");
+    ui->playLevel2->setToolTip("Start Level 2!");
+    ui->playLevel3->setToolTip("Start Level 3!");
     ui->playCustomLevel->setToolTip("Play a custom Level at the specified Path!");
     ui->customLevelPathInput->setToolTip(("Enter the path to a matching txt File to play custom Levels!"));
+    ui->quitButton->setToolTip("Quit Pacman");
 }
 
 mainMenu::~mainMenu(){
@@ -39,6 +48,11 @@ void mainMenu::slotStartLevel3() {
 void mainMenu::slotStartCustomLevel() {
     QString strPath = ui->customLevelPathInput->toPlainText();
     // validate string
+    if (strPath == "") {
+        QErrorMessage *qEM = new QErrorMessage(this);
+        qEM->showMessage("The File Path must not not be empty!");
+        return;
+    }
     gameWindowPtr = new gameWindow(strPath.toStdString());
     gameWindowPtr->show();
 
